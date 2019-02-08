ba62dccf31a6a6d9b801c9508973947c76a560b8
Meng Vang <meng.vang92@yahoo.com>
2019-02-08 16:55:13 -0600

    added data.dat file, reading and display to console

--- File Reader/src/fileReader/FileReader.java
+++ File Reader/src/fileReader/FileReader.java
@@ -1,10 +1,17 @@
 package fileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FileReader {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("src/assets/data.dat");
+		Scanner scanner = new Scanner(file);
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			System.out.println(line);
+		}
 	}
 
 }
