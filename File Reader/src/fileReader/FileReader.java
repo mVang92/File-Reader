32cba4d63f7156c63778137891e9a01b780bd831
Meng Vang <meng.vang92@yahoo.com>
2019-02-08 17:20:37 -0600

    writing to output file, needs work

--- File Reader/src/fileReader/FileReader.java
+++ File Reader/src/fileReader/FileReader.java
@@ -1,17 +1,29 @@
 package fileReader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class FileReader {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		File file = new File("src/assets/data.dat");
 		Scanner scanner = new Scanner(file);
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			System.out.println(line);
+			output(line);
 		}
+
+	}
+
+	public static void output(String line) throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("src/assets/output.txt", "UTF-8");
+		
+		writer.append(line + "\n");
+		writer.close();
 	}
 
 }
