032c76fcf17f377ae68104e44704cbb5eb3094ac
Meng Vang <meng.vang92@yahoo.com>
2019-02-08 18:44:26 -0600

    writing to new file

--- File Reader/src/fileReader/FileReader.java
+++ File Reader/src/fileReader/FileReader.java
@@ -3,27 +3,28 @@ package fileReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class FileReader {
 
-	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		File file = new File("File Reader/src/assets/data.dat");
-		Scanner scanner = new Scanner(file);
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			System.out.println(line);
-			output(line);
-		}
-
-	}
-
-	public static void output(String line) throws FileNotFoundException, UnsupportedEncodingException {
-		PrintWriter writer = new PrintWriter("File Reader/src/assets/output.txt", "UTF-8");
+	public static void main(String[] args) throws FileNotFoundException {
+		try {
+			File input = new File("File Reader/src/assets/data.dat");
+			File output = new File("File Reader/src/assets/output.txt");
+			Scanner scanner = new Scanner(input);
+			PrintWriter printer = new PrintWriter(output);
+			
+			while(scanner.hasNextLine()) {
+				String s = scanner.nextLine();
+				printer.write(s + "\n");
+				// .close() only outputs the first line
+				// Without .flush(), will not output anything
+				printer.flush();
+			}
+			System.out.println("Done.");
 		
-		writer.append(line + "\n");
-		writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 	}
-
 }
